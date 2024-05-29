cd7092da26d5a454c00266dc30f145adbc39792c
Patrik Barisic <wi23b110@technikum-wien.at>
2024-05-29 15:17:58 +0200

    add symbol placement on board and alternating player rounds

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -2,4 +2,27 @@ package org.example;
 
 public class Board {
     private char [][] cells;
+
+    public Board(){
+        cells = new char[3][3];
+    };
+
+    public boolean isCellEmpty(int x, int y){
+        if(x>=0 && x <3 && y >=0 && y<3){
+            if (cells[x][y]==' '){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return false;
+
+    }
+    public void place(int x, int y, char marker){
+        if(isCellEmpty(x,y)){
+            cells[x][y] = marker;
+        }
+    }
+
 }
