d5557601af8c92af2e1cbce6658a2f479de7b8bd
Patrik Barisic <wi23b110@technikum-wien.at>
2024-05-29 16:13:47 +0200

    add tests for class Board

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,19 +1,24 @@
 package org.example;
 
 public class Board {
-    private char [][] cells;
+    public char [][] cells;
 
     public Board(){
         cells = new char[3][3];
+        for(int i = 0; i < cells.length;i++){
+            for(int n = 0; n < cells[i].length;n++){
+                cells[i][n]=' ';
+            }
+        }
     };
 
     public boolean isCellEmpty(int x, int y){
         if(x>=0 && x <3 && y >=0 && y<3){
-            if (cells[x][y]==' '){
-                return true;
-            }
-            else{
+            if (this.cells[x][y]!=' '){
                 return false;
+            }
+            else {
+                return true;
             }
         }
         return false;
